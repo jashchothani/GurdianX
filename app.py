1bc211e0e06ac7e0cca7a3cc1ec6f53fa8b4a4f2
Jash Chothani <jashthakkar77@gmail.com>
2026-04-23 20:46:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ active_qr_tokens = {}
 # DATABASE
 # =============================================================
 def get_db():
-   conn = sqlite3.connect('/data/studx.db', check_same_thread=False)
+   conn = sqlite3.connect('studx.db', check_same_thread=False)
    conn.row_factory = sqlite3.Row
    conn.execute("PRAGMA journal_mode=WAL")
    return conn
